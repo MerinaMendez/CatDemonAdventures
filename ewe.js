ee888923fadac10f82fd9f90ded1641ea641f712
MerinaMendez <107971340+MerinaMendez@users.noreply.github.com>
2023-04-17 21:13:27 +0530

    Add files via upload

---
+++ ewe.js
@@ -0,0 +1,70 @@
+var dia = [
+    "you: another day, another...",
+    "you: distubance in the peace.",
+    "???: SIS!! its time for breakfast!!",  
+    "you: SUDDUP MAYA IM COMING!!!",
+    "maya: GUESS IM EATING YOUR FISH THEN", 
+    "you: OH NO YOUR NOT",
+    "im rushing down the srairs to get my fish",
+    "shes standing rigth next to the fish ready to eat it exactly when im 1.11 cm away from the fish",
+    "maya: *fish eating noises intesifies*",
+    "you: well guess im not eating breakfast today",
+    "maya: guess your not UwU",
+    "[your mind]: oof",
+    "maya:*dabs*",
+    "you walk to school,fishless",
+    "maya:wait, whats the noise?",
+    "you: wha- AHHHH",
+    "maya: DEMON!!!11!",
+    "you get floped of the ground as a demon bursts out of it",
+    "demon: i am the demon of all demons, CHAGANDA!",
+    "vmaya: wait, isnt the demon of all demon saten??",
+    "DEMON CHAGANDA:no, saten is the ruler of demons, i am the demon of all demons, CHAGANDA!",
+    "you: bruh, theres no such thing.",
+    "DEMON CHAGANDA:THERE IS NOW",
+    "maya:EEEEK! SNAKE!",
+    "DEMON CHANGANDA:WHAT?! WHERE?!",
+    "as,CHAGANDA,freaks out you call the police",
+    "police:*silence* what on earth did you get yourselves into.",
+    "you: sos plsss",
+    "the police come and somehow manege to kill ChAgAnDa ........ end of day one"
+];
+
+var WHYRURUNIN = [
+  "welcome.png",
+  "welcome.png",
+  "mayaEating.jpg",
+   "mayaEating.jpg",
+    "mayaEating.jpg",
+    "mayaEating.jpg",
+    "mayaEating.jpg",
+    "mayaEating.jpg",
+    "mayaChewing.jpg",
+    "mayaChewing.jpg",
+    "mayaChewing.jpg",
+    "mayaChewing.jpg",
+    "welcome.png",
+      "welcome.png",
+    "scaredMaya.jpg",
+    "scaredMaya.jpg",
+    "chagandaAppears.jpg",
+    "chaganda.jpg",
+    "scaredMaya.jpg",
+    "chaganda.jpg",
+    "chaganda.jpg",
+    "chaganda.jpg",
+    "scaredMaya.jpg",
+    "chaganda.jpg",
+    "chaganda.jpg",
+    "welcome.png",
+    "chaganda.jpg",
+     "welcome.png"
+];
+var i=0;
+function next(){
+    document.getElementById("iangy").src=WHYRURUNIN[i];
+    document.getElementById("charcoal").textContent ="Next";
+    document.getElementById("h").innerHTML="",
+    document.getElementById("oof").innerHTML= dia[i];
+    i++;
+}
